99df81f1818c252ad6d562b9fce4da187255304d
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-08 21:12:22 +0100

    started using the NASA equations

--- maths.rb
+++ maths.rb
@@ -1,1 +1,25 @@
-angle = 0.5 * asin * (gx / V**2)
+# t = time
+# y = height
+# x = distance
+# V = vertical velocity
+# U = horizontal velocity
+# g = gravitational acceleration
+
+# horizontal component
+# U = U0
+# x = U0t
+
+# vertical component
+# V = V0 - g t
+# y = V0t - .5 g t**2
+
+# highest point
+# V = 0
+# t = V0 / g
+# h = .5 V0 ** 2 / g
+
+# at ground impact
+# y = 0
+# t = 2 V0 / g
+# V = -V0
+
