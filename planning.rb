dc2e0467a737af119d90483ce25cf3a8d4931e68
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-08 16:02:01 +0100

    getting some basic rules in place

--- planning.rb
+++ planning.rb
@@ -1,3 +1,31 @@
+require('pry-byebug')
+
 #describe gravity
 #describe angle
 #describe velocity
+class Physics
+
+  attr_reader( :current_speed )
+
+  def initialize
+    @current_speed = 0
+  end
+
+  def gravity( seconds )
+    current_fall = 9.8 * seconds
+    return current_fall
+  end
+
+  def velocity( unit )
+    @current_speed = unit
+  end
+
+  def distance( time )
+    distance = @current_speed * time
+    return distance
+  end
+
+
+end
+
+
---
+++ specs/planning_spec.rb
@@ -0,0 +1,25 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../planning')
+
+class TestPhysics < MiniTest::Test
+
+  def setup
+    @physics=Physics.new
+  end
+
+  def test_gravity
+    assert_equal( 19.6, @physics.gravity(2) )
+  end
+
+  def test_velocity
+    @physics.velocity( 5 )
+    assert_equal( 5, @physics.current_speed )
+  end
+
+  def test_distance
+    @physics.velocity( 5 )
+    assert_equal( 10, @physics.distance( 2 ))
+  end
+
+end
